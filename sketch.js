2155ed44e3ddfbdf707f3fcc77451057efe201b6
ANAPAULA05082008 <a.batista05@escola.pr.gov.br>
2024-08-26 11:35:39 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function palavrasAleatorias(){
+}
+function InicializaCores (){
+  background("white"); // Cor do fundo.
+  fill("black"); //Cor do texto.
+  textSize(64); //Tamanho da fonte.
+  textAlign(CENTER, CENTER); //Alinha o texto ao centro, x e y.
+
+}
+let palavra;
+function setup() {
+  createCanvas(600, 600); //Tamanho da tela.
+  let palavras = ["anA" ,"pAULA", "bAtIStA"]
+  
+  palavra = random(palavras); //Palavra que será escrita.
+  
+  }
+
+function draw() {
+
+InicializaCores();
+  
+  let quantidade = map(mouseX, 0, width,0, palavra.length);
+  
+  let parcial = palavra.substring(quantidade);-
+  text (parcial,300,300);
+}
